594798f23653db1b139c4744a2a022445c773b32
Niklas Babel <niklas@wane.co>
2023-04-02 19:05:55 +0200

    heading

---
+++ app/controllers/admin/pages_controller.rb
@@ -0,0 +1,6 @@
+class Admin::PagesController < ApplicationController
+    before_action :authenticate_user!, only: [:logout]
+  
+    def dashboard
+    end
+  end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,12 @@ class PagesController < ApplicationController
   def home
   end
 
+  def apply
+  end
+
+  def start
+  end
+
   def logout
     sign_out(current_user)
     redirect_to root_path
