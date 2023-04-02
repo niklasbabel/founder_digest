e5d12f5c3a587364217beb511d37f486ac9155f4
Niklas Babel <niklas@wane.co>
2023-04-02 17:17:49 +0200

    tailwind css hero

---
+++ spec/factories/stake_holder_updates.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :stake_holder_update do
+    content { "MyString" }
+    project { nil }
+  end
+end
---
+++ spec/factories/subscriptions.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :subscription do
+    digest { nil }
+  end
+end
